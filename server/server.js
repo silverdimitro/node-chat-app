004d02b8b30dcfb9907b2494ea7d7051ffe7ed7e
silverdimitro <dumdum.dd9@gmail.com>
2017-03-12 23:10:17 +0530

    data is emiting and receiving from and to server and browser

--- server/server.js
+++ server/server.js
@@ -2,12 +2,30 @@ const path = require('path');
 const express = require('express');
 const port = process.env.PORT || 3000;
 const publicPath=path.join(__dirname,'../public');
-console.log(__dirname + '/../public');
-console.log(publicPath);
+const socketIO = require('socket.io');
+const http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
-  console.log(`'server is runnig at 3000'${port}`);
+io.on('connection',(socket)=>{
+  console.log('new user connected');
+
+  socket.emit('newMessage',{
+    from:'irfan',
+    text:'hello there',
+    createdAt:1234
+  });
+  socket.on('createMessage',(data)=>{
+    console.log('got createMessage from client with ',data);
+  });
+  socket.on('disconnect',()=>{
+    console.log('client disconnected');
+  });
+});
+
+server.listen(port,()=>{
+  console.log(`'server is runnig at '${port}`);
 });
