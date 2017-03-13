d4828ff65682899de3294b325cd8dee54cc352db
silverdimitro <dumdum.dd9@gmail.com>
2017-03-13 19:47:25 +0530

    add users class and updateusers

--- server/server.js
+++ server/server.js
@@ -4,20 +4,35 @@ const port = process.env.PORT || 3000;
 const publicPath=path.join(__dirname,'../public');
 const socketIO = require('socket.io');
 const http = require('http');
-
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 const {generateMessage,generateLocationMessage} = require('./utils/message');
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
+
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('new user connected');
 
-  socket.emit('newMessage',generateMessage('Admin','welcome to chat app'));
-  socket.broadcast.emit('newMessage',generateMessage('Admin','new user joined'));
+  socket.on('join',(params,callback)=>{
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('name and room name are required');
+    }
+    socket.join(params.room);
+    //socket.leave('the office fans');
+    users.removeUser(socket.id);
+    users.addUser(socket.id,params.name,params.room);
 
+    io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+    socket.emit('newMessage',generateMessage('Admin','welcome to chat app'));
+    socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined`));
+
+    callback();
+  });
   socket.on('createMessage',(data,callback)=>{
     console.log('got createMessage from client with ',data);
     io.emit('newMessage',generateMessage(data.from,data.text));
@@ -28,7 +43,15 @@ io.on('connection',(socket)=>{
   });
   socket.on('disconnect',()=>{
     console.log('client disconnected');
+    var user = users.removeUser(socket.id);
+    if(user){
+
+      io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+      io.to(user.room).emit('newMessage',generateMessage('admin',`${user.name} has left`));
+    }
   });
+
+
 });
 
 server.listen(port,()=>{
